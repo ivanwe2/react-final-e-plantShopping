1c01e73d88b1e1584d8e28fed8a824263fb8144a
ivanwe <rusev37@gmail.com>
2025-02-09 15:47:16 -0500

    task 2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const existingItem = state.items.find(item => item.name === action.payload.name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({name: action.payload.name, image: action.payload.image, cost: action.payload.cost, quantity: 1 })
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        
+        if (existingItem) {
+            existingItem.quantit = quantity;
+        }
     },
   },
 });
